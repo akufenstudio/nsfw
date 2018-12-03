29384162d463885b8ce25991d0c3fb410669fabb
Romain Prache <romain@akufen.ca>
2018-12-03 15:43:39 -0500

    UPDATE: YouTube player

--- src/akfn/nsfw/video/youtube/events.js
+++ src/akfn/nsfw/video/youtube/events.js
@@ -32,108 +32,12 @@
  *
  */
 
-/Users/romain/Sites/nsfw/_temp/video/youtube/YouTubePlayer.jsimport EventsManager from 'akfn/nsfw/events/EventsManager';
-import NSFWEvents from 'akfn/nsfw/events/NSFWEvents';
-
-/**
- *  YOUTUBE
- *  PLAYER
- *  
- *  v1.0
- */
-
-class Youtube {
-
-	constructor ( el, id, autoplay = false, loop = false, mute = false ) {
-
-		this.autoplay = autoplay;
-		this.loop = loop;
-		this.mute = mute;
-		this.done = false;
-
-		// bindings
-		this.onPlayerReady = ::this.onPlayerReady;
-		this.onPlayerStateChange = ::this.onPlayerStateChange;
-
-		this.player = new YT.Player(el, {
-			playerVars: {
-				rel:0,
-				controls:2,
-				disablekb:0,
-				modestbranding:1
-			},
-			height: '390',
-			width: '640',
-			autoplay: this.autoplay ? 1 : 0,
-			videoId: id,
-			events: {
-				'onReady': this.onPlayerReady,
-				'onStateChange': this.onPlayerStateChange
-			}
-		});
-
-	}
-
-	onPlayerReady ( event ) {
-
-		event.target.playVideo();
-		
-	}
-
-	onPlayerStateChange ( event ) {
-
-		// 1st play
-		if (event.data == YT.PlayerState.PLAYING && !this.done) {
-			if (!this.autoplay) this.stop();
-			if (this.mute) this.player.mute();
-			this.done = true;
-			
-		// pause
-		} else if (event.data === YT.PlayerState.PAUSED ) {
-
-			EventsManager.emit( NSFWEvents.YoutubePlayer.PAUSE );
-
-		// end
-		} else if (event.data == YT.PlayerState.ENDED) {
-			this.stop();
-			EventsManager.emit( NSFWEvents.YoutubePlayer.PAUSE );
-			EventsManager.emit( NSFWEvents.YoutubePlayer.END );
-			if (this.loop) {
-				this.play();
-			}
-		}
-
-	}
-
-	play () {
-
-		if(this.player) this.player.playVideo();
-
-	}
-
-	pause () {
-
-		this.player.pauseVideo();
-
-	}
-
-	stop () {
-
-		this.player.seekTo(0);
-		this.player.stopVideo();
-
-	}
-
-	dispose () {
-
-		this.player.destroy();
-		this.player = null;
-
-		this.onPlayerReady = 
-		this.onPlayerStateChange = null;
-		
-	}
-
+/Users/romain/Sites/nsfw/_temp/video/youtube/events.jsconst events = {
+	PLAY : "@youtube / PLAY",
+	PAUSE : "@youtube / PAUSE",
+	END : "@youtube / END",
+	SEEK : "@youtube / SEEK",
+	LOADED : "@youtube / LOADED"
 }
 
-export default Youtube;
+export default events;
